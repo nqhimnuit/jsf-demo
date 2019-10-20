19b35e415baf39380f6fbf480e5ea01d098e364b
Minh Quoc Nguyen <nqminhuit@gmail.com>
2019-10-20 16:03:26 +0700

    - add error message to faces context - but label values still shown - expect: label values should not show due to errors.

--- src/main/java/jsf/demo/controller/LoginController.java
+++ src/main/java/jsf/demo/controller/LoginController.java
@@ -1,7 +1,12 @@
 package jsf.demo.controller;
 
 import java.io.Serializable;
+import java.util.Iterator;
+
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import org.apache.logging.log4j.LogManager;
@@ -24,7 +29,11 @@ public class LoginController implements Serializable {
 
 	public void checkUserInfo() throws Exception {
 		if (username.equals(password)) {
-			throw new Exception("Username and password must not be the same");
+			FacesMessage msg = new FacesMessage("Username and password must not be the same");
+			FacesContext context = FacesContext.getCurrentInstance();
+			UIComponent msgComponent = findComponent(context.getViewRoot(), "txtPassword");
+			context.addMessage(msgComponent.getClientId(context), msg);
+			context.renderResponse();
 		}
 	}
 
@@ -44,5 +53,19 @@ public class LoginController implements Serializable {
 	public void setPassword(String password) {
 		LOG.info("setting password = {}", password);
 		this.password = password;
+	}
+
+	private UIComponent findComponent(UIComponent c, String id) {
+		if (id.equals(c.getId())) {
+			return c;
+		}
+		Iterator<UIComponent> kids = c.getFacetsAndChildren();
+		while (kids.hasNext()) {
+			UIComponent found = findComponent(kids.next(), id);
+			if (found != null) {
+				return found;
+			}
+		}
+		return null;
 	}
 }
