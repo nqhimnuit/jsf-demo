6c4ba6e9b2256fe457d548660dd15c32bbad226e
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2019-10-25 08:27:19 +0700

    remove unused imports change session scope to request scoped

--- src/main/java/jsf/demo/controller/LoginController.java
+++ src/main/java/jsf/demo/controller/LoginController.java
@@ -1,25 +1,14 @@
 package jsf.demo.controller;
 
-import java.io.Serializable;
-import java.util.Iterator;
-
-import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * @author AdNovum Informatik AG
  */
 @Named
-@SessionScoped
-public class LoginController implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+@RequestScoped
+public class LoginController {
 
 	private String username;
 
